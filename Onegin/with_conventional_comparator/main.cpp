d523057bd1f8a8d91c2753b29737c19ffee82614
Samircubic <samirsadriev@gmail.com>
2024-09-20 22:51:11 +0300

    Add files via upload

--- Onegin/with_conventional_comparator/main.cpp
+++ Onegin/with_conventional_comparator/main.cpp
@@ -18,19 +18,19 @@ int main (int argc, char *argv[])
         return 0;
     }
 
-    onegin_t *onegin_data = ctor_onegin (); //TODO dtor
+    onegin_t *onegin_data = ctor_onegin ();                     // dtor
 
-    onegin_data->file_name_input = argv[INPUT_FILE_NAME];            // извлечение имени нужного нам файла(для чтения) в структуру данных
-    onegin_data->file_name_output = argv[OUTPUT_FILE_NAME];          // извлечение имени нужного нам файла(для вывода) в структуру данных
-    //TODO вместо названий указатели на файл
-    reed_text (onegin_data);
+    char *file_name_input = argv[INPUT_FILE_NAME];              // извлечение имени нужного нам файла(для чтения) в структуру данных
+    char *file_name_output = argv[OUTPUT_FILE_NAME];            // извлечение имени нужного нам файла(для вывода) в структуру данных
+
+    reed_text (onegin_data, file_name_input);
     assert (onegin_data->text);
 
     creating_pointers (onegin_data);
 
     sort_strings_in_ascending_order (onegin_data);
 
-    output_text_in_file (onegin_data);
+    output_text_in_file (onegin_data, file_name_output);
 
     freedom_of_memory (onegin_data);
 
@@ -49,31 +49,31 @@ onegin_t* ctor_onegin ()
 
 //----------------------------------------------------------------------
 
-void reed_text (onegin_t *onegin_data) //TODO rename
+void reed_text (onegin_t *onegin_data, char *file_name_input)
 {
     assert (onegin_data);
 
-    FILE *file_input = open_file_input (onegin_data);
-    assert (file_input);
+    onegin_data->file_input = open_file_input (onegin_data, file_name_input);
+    assert (onegin_data->file_input);
 
-    measuring_file_len (onegin_data, file_input);
+    measuring_file_len (onegin_data, onegin_data->file_input);
 
-    read_and_check (onegin_data, file_input);
+    read_and_check (onegin_data, onegin_data->file_input);
 
-    fclose (file_input);
+    fclose (onegin_data->file_input);
 }
 
 //----------------------------------------------------------------------
 
-FILE* open_file_input (onegin_t *onegin_data)  //TODO передавай название файла, функция универсальная!!!!
+FILE* open_file_input (onegin_t *onegin_data, char *file_name_input)  //TODO передавай название файла, функция универсальная!!!!
 {
     assert (onegin_data);
-    assert (onegin_data->file_name_input);
+    assert (file_name_input);
 
-    FILE *file_input = fopen (onegin_data->file_name_input, "rb");
-    assert (file_input);
+    onegin_data->file_input = fopen (file_name_input, "rb");
+    assert (onegin_data->file_input);
 
-    return file_input;
+    return onegin_data->file_input;
 }
 
 //----------------------------------------------------------------------
@@ -143,26 +143,28 @@ void creation_pointers_onegin (onegin_t *onegin_data)
     assert (onegin_data);
     assert (onegin_data->text);
 
-    onegin_data->pointers_to_lines = (char**) calloc ((unsigned int) onegin_data->number_lines, sizeof (char *));
+    onegin_data->pointers_to_lines = (pointers_start_end_t*) calloc ((unsigned int) onegin_data->number_lines * 2 + 1, sizeof (pointers_start_end_t*));
     assert (onegin_data->pointers_to_lines);
 
-    onegin_data->pointers_to_lines[0] = &onegin_data->text[0];    //TODO (?)
-
-    int line_number = 1;
-    for (int i = 0; i < onegin_data->len - 2; i++)
+    int j = 0;
+    int line_number = 0;
+    for (int i = 0; i < onegin_data->len - 1; i++)
     {
         if (onegin_data->text[i] == '\r')
         {
-            if (onegin_data->text[i + 2] != ' ' && onegin_data->text[i + 2] != '\r' && onegin_data->text[i + 2] != '\0')
+            if (onegin_data->text[i - 1] != ' ' && onegin_data->text[i - 1] != '\r' && onegin_data->text[i - 1] != '\0')
             {
-                onegin_data->pointers_to_lines[line_number] = &onegin_data->text[i + 2];
+                onegin_data->pointers_to_lines[line_number].pointers_to_lines_start = &onegin_data->text[i - j];
+                onegin_data->pointers_to_lines[line_number].pointers_to_lines_end = &onegin_data->text[i - 1];
                 line_number++;
+                j = -2;
             }
             else
             {
                 onegin_data->number_lines--;
             }
         }
+        j++;
     }
 
     resetting_transfers (onegin_data);
@@ -170,7 +172,7 @@ void creation_pointers_onegin (onegin_t *onegin_data)
 
 //----------------------------------------------------------------------
 
-void resetting_transfers (onegin_t *onegin_data)                                        \\проблема с последней пустой строкой решилась через \0
+void resetting_transfers (onegin_t *onegin_data)        //проблема с последней пустой строкой решилась через \0
 {
     for (int i = 0; i < onegin_data->len; i++)
     {
@@ -203,13 +205,20 @@ void comparing_and_replacing_strings (onegin_t *onegin_data, int comparable)
     assert (onegin_data);
     assert (onegin_data->pointers_to_lines);
 
-    if (conventional_comparator (onegin_data->pointers_to_lines[comparable], onegin_data->pointers_to_lines[comparable + 1]) > 0 )
+    if (conventional_comparator (onegin_data->pointers_to_lines[comparable].pointers_to_lines_start, onegin_data->pointers_to_lines[comparable + 1].pointers_to_lines_start) > 0 )
     {
-        char* temporary = 0;
-        temporary = onegin_data->pointers_to_lines[comparable];
-        onegin_data->pointers_to_lines[comparable] = onegin_data->pointers_to_lines[comparable + 1];
-        onegin_data->pointers_to_lines[comparable + 1] = temporary; //TODO swap
+        swap_pointers (onegin_data, comparable);
     }
+}
+
+//----------------------------------------------------------------------
+
+void swap_pointers (onegin_t *onegin_data, int comparable)
+{
+    char* temporary = 0;
+    temporary = onegin_data->pointers_to_lines[comparable].pointers_to_lines_start;
+    onegin_data->pointers_to_lines[comparable].pointers_to_lines_start = onegin_data->pointers_to_lines[comparable + 1].pointers_to_lines_start;
+    onegin_data->pointers_to_lines[comparable + 1].pointers_to_lines_start = temporary;
 }
 
 //----------------------------------------------------------------------
@@ -256,33 +265,32 @@ int conventional_comparator (char *line_1, char *line_2)
 
 //----------------------------------------------------------------------
 
-void output_text_in_file (onegin_t *onegin_data)
+void output_text_in_file (onegin_t *onegin_data, char *file_name_output)
 {
     assert (onegin_data);
     assert (onegin_data->pointers_to_lines);
 
-    FILE *file_output = open_file_output (onegin_data);
-    assert (file_output);
+    onegin_data->file_output = open_file_output (onegin_data, file_name_output);
+    assert (onegin_data->file_output);
 
     for (int i = 0; i < onegin_data->number_lines; i++)
     {
-        fprintf (file_output ,"%s\n", onegin_data->pointers_to_lines[i]);        //TODO сделать проверку коректности вывода
+        fprintf (onegin_data->file_output ,"%s\n", onegin_data->pointers_to_lines[i].pointers_to_lines_start);
     }
 
-    fclose(file_output);
+    fclose(onegin_data->file_output);
 }
 
 //----------------------------------------------------------------------
 
-FILE* open_file_output (onegin_t *onegin_data)
+FILE* open_file_output (onegin_t *onegin_data, char *file_name_output)
 {
     assert (onegin_data);
-    assert (onegin_data->file_name_output);
+    assert (file_name_output);
 
-    FILE *file_output = NULL;
-    file_output = fopen (onegin_data->file_name_output, "w");
-    assert (file_output);
-    return file_output;
+    onegin_data->file_output = fopen (file_name_output, "w");
+    assert (onegin_data->file_output);
+    return onegin_data->file_output;
 }
 
 //----------------------------------------------------------------------
@@ -300,7 +308,7 @@ void freedom_of_memory (onegin_t *onegin_data)
 
     for (int i = 0; i < onegin_data->number_lines; i++)
     {
-        onegin_data->pointers_to_lines[i] = TOXIC_POINTER;
+        onegin_data->pointers_to_lines[i].pointers_to_lines_start = TOXIC_POINTER;
     }
 
     free (onegin_data->text);
--- Onegin/with_conventional_comparator/main.h
+++ Onegin/with_conventional_comparator/main.h
@@ -1,22 +1,28 @@
 #ifndef MAIN_H
-#define MAIN_H                                              //проверка на активацию
+#define MAIN_H                      //проверка на активацию
 
-const int INPUT_FILE_NAME = 1;
-const int OUTPUT_FILE_NAME = 2;
+const int INPUT_FILE_NAME = 1;      //номер имени файла для ввода в argv
+const int OUTPUT_FILE_NAME = 2;     //номер имени файла для вывода в argv
 
-const char TOXIC_VALUE = 9;
-char* TOXIC_POINTER = 0;
+const char TOXIC_VALUE = 9;         //ядовитая переменная
+char* TOXIC_POINTER = 0;            //ядовитый указатель
 
-const int creating_one_strust = 1;
+const int creating_one_strust = 1;  //создание одной структуры в одном элементе массива
+
+typedef struct pointers_start_end
+{
+    char *pointers_to_lines_start;
+    char *pointers_to_lines_end;
+} pointers_start_end_t;
 
 typedef struct onegin
 {
     char *text;
-    char **pointers_to_lines;
+    pointers_start_end_t *pointers_to_lines;
     int   number_lines;
     int   len;
-    char *file_name_input;
-    char *file_name_output;
+    FILE *file_input;
+    FILE *file_output;
 } onegin_t;
 
 //----------------------------------------------------------------------
@@ -25,7 +31,7 @@ typedef struct onegin
 функция производит все операции связанные с вводом данных
 открытие, считывание длинны, создание стека памяти для текста и передает сам текст из файла в массив
 */
-void        reed_text                       (onegin_t *data);
+void        reed_text                       (onegin_t *data, char *file_name_input);
 
 /*
 
@@ -35,7 +41,7 @@ onegin_t*   ctor_onegin                     ();
 /*
 
 */
-FILE*       open_file_input                 (onegin_t *data);
+FILE*       open_file_input                 (onegin_t *data, char *file_name_input);
 
 /*
 
@@ -65,17 +71,17 @@ void        creation_pointers_onegin        (onegin_t *data);
 /*
 
 */
-void resetting_transfers (onegin_t *onegin_data);
+void        resetting_transfers             (onegin_t *onegin_data);
 
 /*
 
 */
-void        output_text_in_file             (onegin_t *data);
+void        output_text_in_file             (onegin_t *data, char *file_name_output);
 
 /*
 
 */
-FILE*       open_file_output                (onegin_t *data);
+FILE*       open_file_output                (onegin_t *data, char *file_name_output);
 
 /*
 
@@ -86,6 +92,11 @@ void        sort_strings_in_ascending_order (onegin_t *data);
 
 */
 void        comparing_and_replacing_strings (onegin_t *data, int comparable);
+
+/*
+
+*/
+void        swap_pointers                   (onegin_t *onegin_data, int comparable);
 
 /*
 
